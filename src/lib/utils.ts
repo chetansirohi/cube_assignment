5b46ffd4aca78f226967f02dd7a490ca2176f66d
chetansirohi <chetansirohi36@gmail.com>
2024-08-16 10:25:18 +0530

    removed-breaking-changes

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,13 +1,10 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
-import axios from 'axios';
-import { Customer, Photo } from '../types'
+import { Customer } from '../types'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
-
-const API_BASE_URL = 'https://jsonplaceholder.typicode.com';
 
 export const fetchCustomers = async (): Promise<Customer[]> => {
   // In a real scenario, we'd fetch this from an API
@@ -22,9 +19,4 @@ export const fetchCustomers = async (): Promise<Customer[]> => {
     });
   }
   return customers;
-};
-
-export const fetchPhotos = async (): Promise<Photo[]> => {
-  const response = await axios.get(`${API_BASE_URL}/photos?_limit=9`);
-  return response.data;
 };
